7e3802423fe661b0ea56a3b0b2bb02ab3f7cc73c
jesusreluz <jesusmanuelrellu@hotmail.com>
2023-08-03 17:02:24 -0500

    Casi terminado

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', this.window.scrollY > 100);
 });
+
+const menu = document.querySelector('#menu-icon');
+const navlist = document.querySelector('.navlist');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+});
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
