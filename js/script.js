95f270c758892133de9194578919cd3db1fe5898
jesusreluz <jesusmanuelrellu@hotmail.com>
2023-08-04 04:41:00 -0500

    Avance del portafolio

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
+
+const btn_say = document.querySelector('#btn-say-hello');
+const sayhello = document.querySelector('#sayhello');
+const close_say = document.querySelector('#close-say');
+
+btn_say.addEventListener('click', function() {
+    let content = `
+        <pre>
+            <span>Hello World!</span>
+        </pre>
+    `;
+    sayhello.innerHTML = content;
+    close_say.style.display = 'flex'
+});
+
+close_say.addEventListener('click', function() {
+    sayhello.innerHTML = '';
+    close_say.style.display = 'none'
+});
